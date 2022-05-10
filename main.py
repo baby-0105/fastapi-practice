79e9e73173ac5322c778c64943bddf9ab3e17681
baby-0105 <epifa_kikka@icloud.com>
2022-05-10 23:57:17 +0900

    aa

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from typing import Optional
+from typing import Optional, Set, Dict
 
-from fastapi import FastAPI, Path , Query
-from pydantic import BaseModel
-
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_items(
-    *,
-    item_id: int = Path(..., title="The ID of the item to get", ge=1, le=100),
-    q:  str,
-    size: float = Query(..., gt=0, lt=10.5)
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    return results
+@app.post("/index-weights/")
+async def create_index_weights(weights: Dict[int, float]):
+    return weights
