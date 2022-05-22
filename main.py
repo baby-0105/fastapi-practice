0634e48f911d82d0ca8e32fe2c11c1770e48668e
baby-0105 <epifa_kikka@icloud.com>
2022-05-22 21:31:02 +0900

    middle

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 
-@app.get("/items/", tags=["items"])
-async def read_items():
-    return [{"name": "Foo", "price": 42}]
-
-
-@app.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "johndoe"}]
-
-
-@app.get("/elements/", tags=["items"], deprecated=True)
-async def read_elements():
-    return [{"item_id": "Foo"}]
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
